ae04ab2bb3118fc197512b008450ba58d1343f2e
Luisa romero <luisa.romero@ibm.com>
2019-11-24 15:57:19 -0300

    agregando firebase

---
+++ firebase.js
@@ -0,0 +1,14 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyDjt3DJPyrth6T7kaaSJM4WjLxIFitax5Q",
+    authDomain: "koniwe-90b3e.firebaseapp.com",
+    databaseURL: "https://koniwe-90b3e.firebaseio.com",
+    projectId: "koniwe-90b3e",
+    storageBucket: "koniwe-90b3e.appspot.com",
+    messagingSenderId: "937749877815",
+    appId: "1:937749877815:web:9f70c56253ee70b7cbdbb2",
+    measurementId: "G-250Z6ZBSSJ"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
